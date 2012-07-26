83768652bed5bb1e2767d4a550ae8411ae5888f6
Anna Cruz <anna.cruz@gmail.com>
2012-07-26 17:40:55 -0300

    small adjust

--- openvswitch_bridge.sh
+++ openvswitch_bridge.sh
@@ -2,7 +2,7 @@ insmod /root/openvswitch-1.6.1/datapath/linux/openvswitch_mod.ko
 rm -r /usr/local/etc/openvswitch/conf.db
 ovsdb-tool create /usr/local/etc/openvswitch/conf.db /root/openvswitch-1.6.1/vswitchd/vswitch.ovsschema
 ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,manager_options --private-key=db:SSL,private_key --certificate=db:SSL,certificate --bootstrap-ca-cert=db:SSL,ca_cert --pidfile --detach
-ovs-vswitchd --pidfile --deatch
+ovs-vswitchd --pidfile --detach
 ovs-vsctl add-br br0
 ovs-vsctl add-port br0 eth0
 ovs-vsctl set-controller br0 tcp:$1
