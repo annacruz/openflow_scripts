e12b1ad1d5539f150cf66ca06519e4fe50abacc8
Anna Cruz <anna.cruz@gmail.com>
2012-07-28 19:48:26 -0300

    fixing bugs

--- host-creator.sh
+++ host-creator.sh
@@ -5,7 +5,7 @@ hostname=$1
 ip=$2
 switch=$3
 
-sed -e "s/vim\,ssh/vim\,ssh\,tshark\,inetutils-ping\,intetutils-traceroute\,iperf/g" -i /usr/lib/lxc/templates/lxc-ubuntu
+sed -e "s/vim\,ssh\$/vim\,ssh\,tshark\,inetutils-ping\,intetutils-traceroute\,iperf/g" -i  /usr/lib/lxc/templates/lxc-ubuntu
 
 cat > /etc/lxc/lxc-ovs.conf << EOF
 lxc.network.type=veth
